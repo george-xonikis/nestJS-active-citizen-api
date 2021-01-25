0d642a77ecaeb693a973fc8bae803ee97631ca4a
gxo <georgios.xonikis@gmail.com>
2021-01-25 20:49:50 +0100

    user profile fields optionals

--- src/auth/dto/change-user-profile.dto.ts
+++ src/auth/dto/change-user-profile.dto.ts
@@ -1,24 +1,31 @@
-import {IsIn, IsNumber, IsString} from 'class-validator';
+import {IsIn, IsNumber, IsOptional, IsString} from 'class-validator';
 
 export class ChangeUserProfileDto {
     @IsString()
+    @IsOptional()
     firstName: string;
 
     @IsString()
+    @IsOptional()
     lastName: string;
 
     @IsNumber()
+    @IsOptional()
     age: number;
 
     @IsString()
+    @IsOptional()
     bio: string;
 
     @IsString()
+    @IsOptional()
     location: string;
 
     @IsIn(['Male', 'Female'])
+    @IsOptional()
     sex: string;
 
     @IsString()
+    @IsOptional()
     phone: string;
 }
